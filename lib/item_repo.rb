f2c4265384f5bdf96ee3b93cd43fa731f29d1eec
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-04-19 15:36:24 -0600

    Completed item repo methods for iteration 0

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -3,7 +3,7 @@ require 'csv'
 require './lib/sales_engine'
 
 class ItemRepo
-attr_accessor :item_file, :item_array, :data
+  attr_accessor :item_array, :data
   def initialize(sales_engine)
     # (sales_engine)
     @sales_engine = sales_engine
@@ -21,34 +21,96 @@ attr_accessor :item_file, :item_array, :data
   # end
   def load_csv(item_file)
     if item_array.empty?
-      contents = CSV.read './data/items.csv', headers: true, header_converters: :symbol
+      contents = CSV.read item_file, headers: true, header_converters: :symbol
       parse_data(contents)
     end
   end
 
   def create_item_object(data)
     @item_array  <<  Item.new({
-          name: data[0],
-          description: data[1],
-          unit_price:  data[2],
-          created_at: data[3],
-          updated_at: data[4]
+      id: data[0],
+      name: data[1],
+      description: data[2],
+      unit_price:  data[3],
+      created_at: data[4],
+      updated_at: data[5],
+      merchant_id: data[6]
       }, self)
   end
 
   def parse_data(contents)
     contents.each do |row|
       data = []
+      data << row[:id]
       data << row[:name]
       data << row[:description]
-      data << row[:unit_price]
+      data << row[:unit_price].to_i
       data << row[:created_at]
       data << row[:updated_at]
+      data << row[:merchant_id]
       create_item_object(data)
     end
   end
 
   def all
-   item_array
+    item_array
+  end
+  #
+  def find_by_id(id)
+    if item_array.empty?
+      nil
+    else item_array.find do |item|
+      item.id == id
+      end
+    end
+  end
+
+  def find_by_name(name)
+    if item_array.empty?
+      return nil
+    else item_array.find do |item|
+      item.name.downcase == name.downcase
+      end
+    end
+  end
+
+  def find_all_with_description(description)
+    if item_array.empty?
+      item_array
+    else
+      item_array.find_all do |item|
+        item.description.downcase == description.downcase
+      end
+    end
+  end
+
+  def find_all_by_price(unit_price)
+    if item_array.empty?
+      item_array
+    else
+      item_array.find_all do |item|
+        item.unit_price == unit_price
+      end
+    end
+  end
+
+  def find_all_by_price_in_range(range)
+    if item_array.empty?
+      item_array
+    else
+      item_array.find_all do |item|
+        item.unit_price.between?(range.min,range.max)
+      end
+    end
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    if item_array.empty?
+      item_array
+    else
+      item_array.find_all do |item|
+        item.merchant_id == merchant_id
+      end
+    end
   end
 end
--- test/item_repo_test.rb
+++ test/item_repo_test.rb
@@ -20,7 +20,41 @@ class ItemRepoTest < Minitest::Test
 
   def test_case_name
     assert_equal "510+ RealPush Icon Set", ir.all[0].name
-  end 
+  end
+
+  def test_can_find_by_id
+    assert_equal "510+ RealPush Icon Set", ir.find_by_id("263395237").name
+  end
+
+  def test_returns_nil_by_id
+    assert_equal nil, ir.find_by_id(nil)
+  end
+
+  def test_can_find_by_name
+    assert_equal "263395237", ir.find_by_name("510+ RealPush Icon Set").id
+  end
+
+  def test_find_all_with_description_returns_array
+    assert_equal "263453001",ir.find_all_with_description("Coffee Mug Holder, Wall Decor")[0].id
+  end
+
+  def test_find_all_with_description_returns_items_with_price
+    assert_equal 48, ir.find_all_by_price(2000).count
+  end
+
+  def test_find_all_with_range_returns_items_within_range
+    assert_equal 64, ir.find_all_by_price_in_range(1000..1050).count
+  end
+
+  def test_finds_all_from_user_id
+    assert_equal 1, ir.find_all_by_merchant_id('12335009').count
+  end
+
+  
+  #
+  # def test_cannot_find_nil_name
+  #   assert_equal nil, ir.find_by_name(nil)
+  # end
 
   # def test_csv_loads_names
   #   assert_equal "510+ RealPush Icon Set", item_repo.load_csv()[0]
