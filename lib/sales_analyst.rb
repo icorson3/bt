e54eed00b66730aa22f9f13640c2f84d636c9b9d
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-04-20 23:27:16 -0600

    Completed ugly rough draft of iteration 1

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,7 +1,6 @@
 class SalesAnalyst
 
-  attr_reader :sales_engine, :price_array
-
+  attr_reader :sales_engine
   def initialize(sales_engine)
     @sales_engine = sales_engine
   end
@@ -10,20 +9,58 @@ class SalesAnalyst
     (sales_engine.items.item_array.count.to_f/sales_engine.merchants.merchant_array.count.to_f).round(2)
   end
 
+  def average_items_per_merchant_standard_deviation
+    avg = average_items_per_merchant
+    number_items_array = sales_engine.merchants.merchant_array.map do |merchant|
+      merchant.items.count
+    end
+    sample_variance = number_items_array.reduce(0) do |total,merchant_avg|
+      total + ((merchant_avg - avg) ** 2)
+    end/(number_items_array.length - 1).to_f
+
+    Math.sqrt(sample_variance).round(2)
+  end
+
   def average_item_price_for_merchant(merchant_id)
-    #find all items that the merchant has
     merchant_items = sales_engine.merchants.find_by_id(merchant_id).items
-    @price_array = merchant_items.map do |merchant_item|
+    price_array = merchant_items.map do |merchant_item|
       merchant_item.unit_price
     end.reduce(:+)
     price_array/merchant_items.count
   end
 
-  def average_average_price_for_merchant
-    require "pry"; binding.pry
-    average_price_each_merchant = sales_engine.merchants
-    #sum unit price of merchant's items
-    #divide by number of items merchant has
+  def merchants_with_high_item_count
+    std_deviation = average_items_per_merchant_standard_deviation
+    high_item_count = average_items_per_merchant + std_deviation
+    sales_engine.merchants.merchant_array.find_all do |merchant|
+      merchant.items.length > high_item_count
+    end
   end
 
+  def average_average_price_for_merchant
+    total_sum_average_shop_prices = sales_engine.merchants.merchant_array.map do |merchant|
+      average_item_price_for_merchant(merchant.id)
+    end.reduce(:+)
+    (total_sum_average_shop_prices/sales_engine.merchants.merchant_array.count).round(2)
+  end
+
+  def average_item_price_standard_deviation
+    avg = average_average_price_for_merchant
+    unit_price_array = sales_engine.items.item_array.map do |item|
+      item.unit_price
+    end
+    sample_variance = unit_price_array.reduce(0) do |total,price_avg|
+      total + ((price_avg - avg) ** 2)
+    end/(unit_price_array.length - 1).to_f
+
+    Math.sqrt(sample_variance).round(2)
+  end
+
+  def golden_items
+    double_deviation = (average_average_price_for_merchant * 2)
+    high_price = double_deviation + average_average_price_for_merchant
+    sales_engine.items.item_array.find_all do |item|
+      item.unit_price > high_price
+    end
+  end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -17,11 +17,29 @@ attr_reader :se, :sa
     assert_equal 2.88, sa.average_items_per_merchant
   end
 
+  def test_returns_merchants_with_highest_item_counts
+    assert_equal "Keckenbauer", sa.merchants_with_high_item_count[0].name
+  end
+
   def test_average_item_price_per_merchant
     assert_equal BigDecimal, sa.average_item_price_for_merchant(12334105).class
   end
 
   def test_average_average_price_per_merchant
-    assert_equal BigDecimal, sa.average_average_price_for_merchant
+    assert_equal BigDecimal, sa.average_average_price_for_merchant.class
   end
+
+  def test_standard_deviation_works_for_items_per_merchant
+    assert_equal 3.26, sa.average_items_per_merchant_standard_deviation
+  end
+
+  def test_standard_deviation_works_for_item_price
+    assert_equal 2902.69, sa.average_item_price_standard_deviation
+  end
+
+  def test_highest_priced_items_returned_from_golden_items
+    assert_equal 38, sa.golden_items.count
+    assert_equal "Solid American Black Walnut Trestle Table", sa.golden_items[37].name
+  end
+
 end
